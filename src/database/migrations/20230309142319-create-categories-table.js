a2f9bf04fa7aec271cd02b2b471cd927b7aa87ec
lucasdmmc <lucas.dmmc@hotmail.com>
2023-03-09 11:46:38 -0300

    Criando o primeiro Resource

--- src/database/migrations/20230309142319-create-categories-table.js
+++ src/database/migrations/20230309142319-create-categories-table.js
@@ -2,20 +2,33 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING
+      },
+      position: {
+        allowNull: false,
+        type: Sequelize.DataTypes.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      }
+    })
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('categories')
   }
 };
